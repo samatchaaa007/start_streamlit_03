9cbc3be811900571e03c5a8086eb7351567b2bc3
samatchaaa007 <beingsdeemark@gmail.com>
2025-04-01 22:43:33 +0700

    Update log_utils.py

--- log_utils.py
+++ log_utils.py
@@ -2,17 +2,16 @@ def send_log_to_google_form(username, fullname, browser, page, event, ip):
     import requests
     from datetime import datetime
 
-    #url = "https://docs.google.com/forms/d/e/1FAIpQLSfp2aRm1XAGkRL0K5y87680BYcko8aGxJyhpNLsrdaxLbl6Sg/viewform?usp=header"
-    url = "https://docs.google.com/forms/d/e/1FAIpQLSfp2aRm1XAGkRL0K5y87680BYcko8aGJyhNLsrdaxLbl6Sg/formResponse"
-
+    url = "https://docs.google.com/forms/d/e/1FAIpQLSfp2aRm1XAGkRL0K5y87680BYcko8aGxJyhpNLsrdaxLbI65g/formResponse"
+    
     form_data = {
-        "entry.1234567890": username,
-        "entry.2345678901": fullname,
-        "entry.3456789012": browser,
-        "entry.4567890123": page,
-        "entry.5678901234": event,
-        "entry.6789012345": ip,
-        "entry.7890123456": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        "entry.36918558": username,
+        "entry.94384076": fullname,
+        "entry.1474490753": browser,
+        "entry.264051270": page,
+        "entry.2013583068": event,
+        "entry.543729100": ip,
+        "entry.526048478": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
 
     response = requests.post(url, data=form_data)
